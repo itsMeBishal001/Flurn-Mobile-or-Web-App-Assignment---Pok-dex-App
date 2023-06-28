c342f80a03da1660a9a8c9347aac86ca73885eac
BISHAL DATTA <dattabishal001@gmail.com>
2023-06-28 16:48:41 +0530

    initial setup

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*
+AppLayout 
+  
+*/
+const App=()=>{
+return <>
+<div>hey i am here </div>
+</>
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
