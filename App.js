0c7541103855db0aeae65683f1c394115b681cdf
BISHAL DATTA <dattabishal001@gmail.com>
2023-06-28 19:34:12 +0530

    Search functionality added
    have to make dynamic

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Search from "./Search";
 
 /*
 AppLayout 
-  
+  serch component 
+  api call after hitting serch
 */
 const App=()=>{
 return <>
-<div>hey i am here </div>
+<Search/>
 </>
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ Search.js
@@ -0,0 +1,8 @@
+const Search=()=>{
+    return <>
+    <input value={"hii"}></input>
+    <button>Serch</button>
+    </>
+}
+
+export default Search;
